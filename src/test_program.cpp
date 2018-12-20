226dddee4be160f10e360564fd42677c0793bc1b
CorneVgit <corneverduin@hotmail.com>
2018-12-20 23:38:01 +0100

    added corresponding header

--- src/test_program.cpp
+++ src/test_program.cpp
@@ -1,25 +1,6 @@
-#include <termios.h>
-#include <linux/serial.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <string>
-#include <iostream>
-
-#define MAX_TX 256
-#define MAX_RX 1024
-
-#define TX_ID 2
-#define TX_INSTRUCTION 4
-#define TX_ADDRESS 5
-#define TX_VALUE 6
-#define TX_LENGTH 3
+#include <test_program.hpp>
 
 int fd = -1;
-
-int WB(unsigned char id, unsigned char address, unsigned char value);
 
 int main(int argc, char **argv)
 {
