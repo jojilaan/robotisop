934d48732fc4ee2519654d8707034dc17c63961f
Corné Verduin <corneverduin@hotmail.com>
2018-12-20 15:12:40 +0100

    added test

---
+++ src/test_program.cpp
@@ -0,0 +1,29 @@
+#include <termios.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  struct termios newio;
+  struct serial_struct serinfo;
+
+  std::string portname = "/dev/ttyUSB0";
+  double baudrate = 1000000.0;
+
+  int fd = -1;
+  
+  fd = open(portname.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
+  {
+    if (fd < 0) {
+      std::cout << "FAILED" << std::endl;
+    } else {
+      std::cout << "Succes" << std::endl;
+    }
+  }
+
+  if (fd >= 0) {
+    close(fd);
+  }
+
+  return 0;
+}
