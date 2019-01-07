045437131c4f52ed2b17519d322928d6a1882773
corneVgit <corneverduin@hotmail.com>
2019-01-07 11:59:14 +0100

    updated main

--- src/test_program.cpp
+++ src/test_program.cpp
@@ -1,7 +1,28 @@
 #include <test_program.hpp>
 #include <darwin_connection.hpp>
+#include <sys/time.h>
 
 int fd = -1;
+
+double getCurrentTime()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	return ((double)tv.tv_sec * 1000.0 + (double)tv.tv_usec / 1000.0);
+}
+
+void sleepCustom(double msec)
+{
+	double start_time = getCurrentTime();
+	double curr_time = start_time;
+
+	do
+	{
+		usleep((start_time + msec) - curr_time);
+		curr_time = getCurrentTime();
+	} while (curr_time - start_time < msec);
+}
 
 int main()
 {
@@ -9,12 +30,43 @@ int main()
 	connection.openConnection();
 	Packet packet = Packet(200, Packet::WRITE, 24, static_cast<unsigned char>(1));
 	packet.build();
-	if (connection.transferPacket(packet)) {
+	if (connection.transferPacket(packet))
+	{
 		std::cout << "SUCCES" << std::endl;
-		packet = Packet(200, Packet::WRITE, 26, (int)((((255 & 0xFF) >> 3) << 10) | (((128 & 0xFF) >> 3) << 5) | ((0 & 0xFF) >> 3)));
+		packet = Packet(200, Packet::WRITE, 26, (int)((((0 & 0xFF) >> 3) << 10) | (((128 & 0xFF) >> 3) << 5) | ((255 & 0xFF) >> 3)));
 		packet.build();
 		connection.transferPacket(packet);
+
+		sleepCustom(300);
 	}
+
+	packet = Packet(1, Packet::WRITE, 24, 0);
+	packet.build();
+	if (connection.transferPacket(packet))
+		std::cout << "SUCCES" << std::endl;
+	packet = Packet(3, Packet::WRITE, 24, 0);
+	packet.build();
+	if (connection.transferPacket(packet))
+		std::cout << "SUCCES" << std::endl;
+	packet = Packet(5, Packet::WRITE, 24, 0);
+	packet.build();
+	if (connection.transferPacket(packet))
+		std::cout << "SUCCES" << std::endl;
+
+	packet = Packet(2, Packet::WRITE, 28, static_cast<unsigned char>(8));
+	packet.build();
+	if (connection.transferPacket(packet))
+		std::cout << "SUCCES" << std::endl;
+	packet = Packet(4, Packet::WRITE, 28, static_cast<unsigned char>(8));
+	packet.build();
+	if (connection.transferPacket(packet))
+		std::cout << "SUCCES" << std::endl;
+	packet = Packet(6, Packet::WRITE, 28, static_cast<unsigned char>(8));
+	packet.build();
+	if (connection.transferPacket(packet))
+		std::cout << "SUCCES" << std::endl;
+
+	usleep(50000);
 
 	connection.closeConnection();
 }
