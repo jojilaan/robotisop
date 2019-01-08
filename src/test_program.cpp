891fb853fbe6f11aedfff36f8d060191624a5db5
corneVgit <corneverduin@hotmail.com>
2019-01-08 11:02:24 +0100

    refactored

--- src/test_program.cpp
+++ src/test_program.cpp
@@ -109,54 +109,44 @@ void *run_deadline(void *data)
 	connection.openConnection();
 
 	Packet packet = Packet(200, Packet::WRITE, 24, static_cast<unsigned char>(1));
-	packet.build();
 	if (connection.transferPacket(packet))
 	{
 		std::cout << "SUCCES" << std::endl;
 		packet = Packet(200, Packet::WRITEW, 26, (int)((((0 & 0xFF) >> 3) << 10) | (((128 & 0xFF) >> 3) << 5) | ((255 & 0xFF) >> 3)));
-		packet.build();
-		connection.transferPacket(packet);
+			connection.transferPacket(packet);
 
 		sleepCustom(300);
 	}
 
 	packet = Packet(1, Packet::WRITEW, 24, 0);
-	packet.build();
 	if (connection.transferPacket(packet))
 		std::cout << "SUCCES" << std::endl;
 	packet = Packet(3, Packet::WRITEW, 24, 0);
-	packet.build();
 	if (connection.transferPacket(packet))
 		std::cout << "SUCCES" << std::endl;
 	packet = Packet(5, Packet::WRITEW, 24, 0);
-	packet.build();
 	if (connection.transferPacket(packet))
 		std::cout << "SUCCES" << std::endl;
 
 	packet = Packet(2, Packet::WRITE, 28, static_cast<unsigned char>(8));
-	packet.build();
 	if (connection.transferPacket(packet))
 		std::cout << "SUCCES" << std::endl;
 	packet = Packet(4, Packet::WRITE, 28, static_cast<unsigned char>(8));
-	packet.build();
 	if (connection.transferPacket(packet))
 		std::cout << "SUCCES" << std::endl;
 	packet = Packet(6, Packet::WRITE, 28, static_cast<unsigned char>(8));
-	packet.build();
 	if (connection.transferPacket(packet))
 		std::cout << "SUCCES" << std::endl;
 
 	while (!done)
 	{
 		packet = Packet(2, Packet::WRITEW, 30, 400);
-		packet.build();
-		connection.transferPacket(packet);
+			connection.transferPacket(packet);
 
 		usleep(1000000);
 
 		packet = Packet(2, Packet::WRITEW, 30, 2000);
-		packet.build();
-		connection.transferPacket(packet);
+			connection.transferPacket(packet);
 
 		sched_yield();
 	}
