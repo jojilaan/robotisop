c0b5408653331594729dea9e1e10dfcbd954723c
corneVgit <corneverduin@hotmail.com>
2019-01-11 13:03:14 +0100

    added moveServo function

--- src/test_program.cpp
+++ src/test_program.cpp
@@ -21,6 +21,21 @@ void sleepCustom(double msec)
 	} while (curr_time - start_time < msec);
 }
 
+bool moveServo(Connection &connection, unsigned char id, int value)
+{
+	Packet packet = Packet(id, Packet::WRITEW, P_GOAL_POSITION_L, value);
+	if (connection.transferPacket(packet))
+	{
+		std::cout << "moveServo(" << id << ", " << value << ") SUCCES" << std::endl;
+		return true;
+	}
+	else
+	{
+		std::cout << "moveServo(" << id << ", " << value << ") FAILED" << std::endl;
+		return false;
+	}
+}
+
 int main()
 {
 	Connection connection = Connection("/dev/ttyUSB0");
@@ -83,67 +98,40 @@ int main()
 
 	usleep(50000);
 
-	packet = Packet(ID_R_SHOULDER_PITCH, Packet::WRITEW, P_GOAL_POSITION_L, 1948);
-	if (connection.transferPacket(packet))
-		std::cout << "R_SHOULDER_PITCH: SUCCES" << std::endl;
-	packet = Packet(ID_R_SHOULDER_ROLL, Packet::WRITEW, P_GOAL_POSITION_L, 1560);
-	if (connection.transferPacket(packet))
-		std::cout << "R_SHOULDER_ROLL: SUCCES" << std::endl;
-	packet = Packet(ID_R_ELBOW, Packet::WRITEW, P_GOAL_POSITION_L, 980);
-	if (connection.transferPacket(packet))
-		std::cout << "R_ELBOW: SUCCES" << std::endl;
+	// Right arm init
+	moveServo(connection, ID_R_SHOULDER_PITCH, 1948);
+	moveServo(connection, ID_R_SHOULDER_ROLL, 1560);
+	moveServo(connection, ID_R_ELBOW, 980);
 
-	packet = Packet(ID_L_SHOULDER_PITCH, Packet::WRITEW, P_GOAL_POSITION_L, 2127);
-	if (connection.transferPacket(packet))
-		std::cout << "L_SHOULDER_PITCH: SUCCES" << std::endl;
-	packet = Packet(ID_L_SHOULDER_ROLL, Packet::WRITEW, P_GOAL_POSITION_L, 2534);
-	if (connection.transferPacket(packet))
-		std::cout << "L_SHOULDER_ROLL: SUCCES" << std::endl;
-	packet = Packet(ID_L_ELBOW, Packet::WRITEW, P_GOAL_POSITION_L, 3107);
-	if (connection.transferPacket(packet))
-		std::cout << "L_ELBOW: SUCCES" << std::endl;
+	// Left arm init
+	moveServo(connection, ID_L_SHOULDER_PITCH, 2127);
+	moveServo(connection, ID_L_SHOULDER_ROLL, 2534);
+	moveServo(connection, ID_L_ELBOW, 3107);
 
 	usleep(1000000);
 
-	packet = Packet(ID_R_SHOULDER_PITCH, Packet::WRITEW, P_GOAL_POSITION_L, 3451);
-	if (connection.transferPacket(packet))
-		std::cout << "R_SHOULDER_PITCH: SUCCES" << std::endl;
-
-	packet = Packet(ID_L_SHOULDER_PITCH, Packet::WRITEW, P_GOAL_POSITION_L, 625);
-	if (connection.transferPacket(packet))
-		std::cout << "L_SHOULDER_PITCH: SUCCES" << std::endl;
+	// First position
+	moveServo(connection, ID_R_SHOULDER_PITCH, 3451);
+	moveServo(connection, ID_L_SHOULDER_PITCH, 625);
 
 	usleep(1000000);
 
-	packet = Packet(ID_R_ELBOW, Packet::WRITEW, P_GOAL_POSITION_L, 1480);
-	if (connection.transferPacket(packet))
-		std::cout << "R_ELBOW: SUCCES" << std::endl;
-
-	packet = Packet(ID_L_ELBOW, Packet::WRITEW, P_GOAL_POSITION_L, 2621);
-	if (connection.transferPacket(packet))
-		std::cout << "L_ELBOW: SUCCES" << std::endl;
+	// Second position
+	moveServo(connection, ID_R_ELBOW, 1480);
+	moveServo(connection, ID_L_ELBOW, 2621);
 
 	for (size_t i = 0; i < 5; i++)
 	{
 		usleep(500000);
 
-		packet = Packet(ID_R_SHOULDER_ROLL, Packet::WRITEW, P_GOAL_POSITION_L, 1240);
-		if (connection.transferPacket(packet))
-			std::cout << "R_SHOULDER_ROLL: SUCCES" << std::endl;
-
-		packet = Packet(ID_L_SHOULDER_ROLL, Packet::WRITEW, P_GOAL_POSITION_L, 2850);
-		if (connection.transferPacket(packet))
-			std::cout << "L_SHOULDER_ROLL: SUCCES" << std::endl;
+		// Clap
+		moveServo(connection, ID_R_SHOULDER_ROLL, 1240);
+		moveServo(connection, ID_L_SHOULDER_ROLL, 2850);
 
 		usleep(500000);
 
-		packet = Packet(ID_R_SHOULDER_ROLL, Packet::WRITEW, P_GOAL_POSITION_L, 1560);
-		if (connection.transferPacket(packet))
-			std::cout << "R_SHOULDER_ROLL: SUCCES" << std::endl;
-
-		packet = Packet(ID_L_SHOULDER_ROLL, Packet::WRITEW, P_GOAL_POSITION_L, 2534);
-		if (connection.transferPacket(packet))
-			std::cout << "L_SHOULDER_ROLL: SUCCES" << std::endl;
+		moveServo(connection, ID_R_SHOULDER_ROLL, 1560);
+		moveServo(connection, ID_L_SHOULDER_ROLL, 2534);
 	}
 
 	usleep(50000);
